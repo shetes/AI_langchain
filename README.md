ebf13d5a6f2b29700f56ccec7e956ffe086b4cb0
Maanas Shete <maanas.shete@gmail.com>
2025-11-16 22:13:23 -0500

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# AI_langchain
-AI Langchain
--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
 def main():
     print("Hello from ai-langchain!")
-    print(os.getenv("OPENAI_API_KEY"))
 
 
 if __name__ == "__main__":
